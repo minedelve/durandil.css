7d9b9369907a26a00ac2c3910397f27d83c2ad2d
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2023-12-09 18:21:02 +0100

    add tsconfig and create durandilConfig function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-export function durandil() {
-	return {
-		name: 'durandil-css-vite',
-		async configResolved() {
-			console.log('@durandil/css run ! = configResolved');
-		},
-		async configureServer(server: any) {
-			server.watcher.add('./');
-			console.log('@durandil/css run ! = configureServer');
-		}
-	};
-}
+export * from "./vite.process";
---
+++ src/vite.process.ts
@@ -0,0 +1,12 @@
+export function durandilConfig() {
+  return {
+    name: "durandil-css-vite",
+    async configResolved() {
+      console.log("@durandil/css run ! = configResolved");
+    },
+    async configureServer(server: any) {
+      server.watcher.add("./");
+      console.log("@durandil/css run ! = configureServer");
+    },
+  };
+}
