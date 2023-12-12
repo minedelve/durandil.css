3f6a8122163741fb412611119580f120bae4fe32
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2023-12-12 20:21:08 +0100

    add function for minify css and new export /min

--- src/vite.process.ts
+++ src/vite.process.ts
@@ -4,8 +4,13 @@ import path from "path";
 import setRootCSS from "./libs/root"; // TEMP
 import setClassTheme from "./libs/theme";
 import preset from "./presets/default";
+import { minimifyCss } from "./style";
 
 const pathCss = path.resolve(`node_modules/@durandil/css/build`, "style.css");
+const pathCssMin = path.resolve(
+  `node_modules/@durandil/css/build`,
+  "style.min.css"
+);
 
 export function durandilConfig() {
   return {
@@ -20,6 +25,7 @@ export function durandilConfig() {
       console.log(response);
 
       fs.writeFile(pathCss, response);
+      fs.writeFile(pathCssMin, minimifyCss(response));
     },
     async configureServer(server: any) {
       server.watcher.add("./");
