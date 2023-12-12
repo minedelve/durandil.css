137fea72104fb9c014740291210483c841e89a80
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2023-12-12 20:39:38 +0100

    WIP read config file

--- src/vite.process.ts
+++ src/vite.process.ts
@@ -11,18 +11,30 @@ const pathCssMin = path.resolve(
   `node_modules/@durandil/css/build`,
   "style.min.css"
 );
+const rootDir = process.cwd();
+export const configPath = path.resolve(rootDir, "durandil.config.js");
 
 export function durandilConfig() {
   return {
     name: "durandil-css-vite",
     async configResolved() {
       console.log("@durandil/css run ! = configResolved");
+      const file = await fs.readFile(configPath, "utf-8");
+      const defaultExportMatch = file.match(/export\s+default\s+(\{[^]*?\});/);
+      let defaultValue = null;
 
+      if (defaultExportMatch && defaultExportMatch.length > 1) {
+        const exportCode = `return ${defaultExportMatch[1]}`;
+        const defaultExportFunction = new Function(exportCode);
+        defaultValue = defaultExportFunction();
+      }
+
+      console.log("defaultValue", defaultValue, defaultValue.theme);
       let response = "";
       response += setRootCSS(preset);
       response += setClassTheme(preset);
 
-      console.log(response);
+      // console.log(response);
 
       fs.writeFile(pathCss, response);
       fs.writeFile(pathCssMin, minimifyCss(response));
