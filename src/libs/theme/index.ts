f4faccbd1d11f4ce07ae203292e82ba66ecb1e4b
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2023-12-12 20:17:16 +0100

    fix theme class css for propertie object or string

--- src/libs/theme/index.ts
+++ src/libs/theme/index.ts
@@ -5,24 +5,22 @@ function setClassTheme(settings: Config) {
   let response = "";
 
   if (settings.colors) {
-    for (const [key, value] of Object.entries(settings.colors)) {
-      if (typeof value === "string") {
-        response += `.bg\:${key} {\n`;
-        response += css({
-          key: "background-color",
-          value: `var(--color-${key})`,
-          important: true,
-        });
-        response += "}\n";
+    for (const [key] of Object.entries(settings.colors)) {
+      response += `.bg\:${key} {\n`;
+      response += css({
+        key: "background-color",
+        value: `var(--color-${key})`,
+        important: true,
+      });
+      response += "}\n";
 
-        response += `.text\:${key} {\n`;
-        response += css({
-          key: "color",
-          value: `var(--color-${key})`,
-          important: true,
-        });
-        response += "}\n";
-      }
+      response += `.text\:${key} {\n`;
+      response += css({
+        key: "color",
+        value: `var(--color-${key})`,
+        important: true,
+      });
+      response += "}\n";
     }
   }
 
--- src/vite.process.ts
+++ src/vite.process.ts
@@ -1,6 +1,11 @@
+import fs from "fs/promises";
+import path from "path";
+
 import setRootCSS from "./libs/root"; // TEMP
 import setClassTheme from "./libs/theme";
 import preset from "./presets/default";
+
+const pathCss = path.resolve(`node_modules/@durandil/css/build`, "style.css");
 
 export function durandilConfig() {
   return {
@@ -13,6 +18,8 @@ export function durandilConfig() {
       response += setClassTheme(preset);
 
       console.log(response);
+
+      fs.writeFile(pathCss, response);
     },
     async configureServer(server: any) {
       server.watcher.add("./");
